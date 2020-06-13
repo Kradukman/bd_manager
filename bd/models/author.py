4afcf8aae5d21eb9bfc2e8b1a059b8796dffc2df
dag <dag@odoo.com>
2020-06-13 14:47:02 +0200

    Fixing author alias add

--- bd/models/author.py
+++ bd/models/author.py
@@ -21,5 +21,6 @@ class AuthorAlias(models.Model):
     def write(self, vals):
         res = super(AuthorAlias, self).write(vals)
         if ('author_id' in vals):
-            self.bd_ids.write({'author_ids': [[6, False, (author_alias.author_id.id for author_alias in self.bd_ids.author_alias_ids)]]})
+            self.bd_ids.write({'author_ids': [[6, 0, 
+                (author_alias.author_id.id for author_alias in self.bd_ids.author_alias_ids if author_alias.author_id)]]})
         return res
